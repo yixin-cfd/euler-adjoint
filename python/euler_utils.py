7d49f3ff4ee0d1673422d77e7703c490e7f00451
Dylan Jude <djude@umd.edu>
2016-10-12 23:27:32 -0400

    added default input dictionary

---
+++ python/euler_utils.py
@@ -0,0 +1,16 @@
+
+
+def defaults():
+    return { "mach" : 0.3,
+             "ilhs" : 1,
+             "resid" : 10,
+             "steps" : 100,
+             "cfl" : 10.0,
+             "aoa" : 0.0,
+             "bcs" : [
+                 { "face": "jmin", "type": "periodic", "j": [0,    0], "k": [0,  -1] },
+                 { "face": "jmax", "type": "periodic", "j": [-1,  -1], "k": [0,  -1] },
+                 { "face": "kmin", "type": "wall"    , "j": [0,   -1], "k": [0,   0] },
+                 { "face": "kmax", "type": "farfield", "j": [0,   -1], "k": [-1, -1] }
+             ]
+             }
