f6f052af0142997493782fcf1c2959aff0c9fb83
dylan <djude@umd.edu>
2016-10-21 09:37:39 -0400

    added python 2d mesh plotter

--- python/grid_utils.py
+++ python/grid_utils.py
@@ -1,5 +1,5 @@
+from matplotlib import pyplot as plt
 import numpy as np
-
 
 def load_grid(filename):
     with open(filename, "r") as f:
@@ -10,3 +10,17 @@ def load_grid(filename):
     reordered[:, :, 0] = data[0, :, :]
     reordered[:, :, 1] = data[1, :, :]
     return reordered
+
+
+def plot2d(filename):
+    with open(filename, "r") as f:
+        jtot, ktot = [int(x) for x in f.readline().split()]
+    
+    data = np.loadtxt(filename, skiprows=1)
+    data = data.reshape((2, ktot, jtot))
+    
+    for k in range(ktot):
+        plt.plot(data[0,k,:], data[1,k,:], '-b')
+    for j in range(jtot):
+        plt.plot(data[0,:,j], data[1,:,j], '-b')
+    plt.show()
